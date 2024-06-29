5dccbf288d4317dccf92ddb3c9323c25843c3a43
Bahram Malaekeh <Mithrandir21@users.noreply.github.com>
2024-06-29 19:36:08 +0100

    Fixed unit tests

--- feature/shows/src/commonTest/kotlin/pm/bam/mbc/feature/shows/ui/shows/ShowsViewModelTest.kt
+++ feature/shows/src/commonTest/kotlin/pm/bam/mbc/feature/shows/ui/shows/ShowsViewModelTest.kt
@@ -26,7 +26,7 @@ import kotlin.test.Test
 private val showFlow = MutableStateFlow<List<Show>>(emptyList())
 private val baseShow = Show(
     1, "name", "desc", "url", listOf("images"), listOf(Categories.COMEDY), listOf(1, 2, 3), schedule = listOf(
-        ShowSchedule(1, EventStatus.ACTIVE, ShowVenues.MB1, LocalDateTime(2021, 1, 1, 1, 1), LocalDateTime(2021, 1, 1, 1, 1))
+        ShowSchedule(1, EventStatus.ACTIVE, ShowVenues.MB1, LocalDateTime(2021, 1, 1, 1, 1), LocalDateTime(2021, 1, 1, 1, 1), listOf(1, 2, 3))
     )
 )
 
